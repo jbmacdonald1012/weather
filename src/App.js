5d0b1dc960c648683816d794e77d168e01e69c2f
jmacdonald <jmacdonald@conservice.com>
2022-10-20 15:59:02 -0600

    Import background images into App.js

--- src/App.js
+++ src/App.js
@@ -1,3 +1,9 @@
+import cloudyBg from './assets/partlyCloudy.jpg';
+import rainyBg from './assets/rainy.jpg';
+import snowBg from './assets/snowing.jpg';
+import sunBg from './assets/sunny.jpg';
+import stormBg from './assets/thunderstorms.jpg'
+
 function App() {
   return (
     <div className="app">
