b2ea1f03ae9bdeb89cd1402abae57614630043ef
jmacdonald <jmacdonald@conservice.com>
2022-10-24 16:16:39 -0600

    Add sections for city info and temperature

--- src/App.js
+++ src/App.js
@@ -2,12 +2,31 @@ import cloudyBg from './assets/partlyCloudy.jpg';
 import rainyBg from './assets/rainy.jpg';
 import snowBg from './assets/snowing.jpg';
 import sunBg from './assets/sunny.jpg';
-import stormBg from './assets/thunderstorms.jpg'
+import stormBg from './assets/thunderstorms.jpg';
+import partlyCloudyIcon from './assets/partlyCloudyIcon.png';
 
 function App() {
   return (
     <div className="app" style={{backgroundImage: `url(${sunBg})`}}>
-      
+      <div className="overlay">
+        <div className="container">
+          <div className="section section__inputs">
+            <input type="text" name="city" placeholder="Enter city..." />
+            <button>Search</button>
+          </div>
+
+          <div className="section section__temperature">
+            <div className="icon">
+              <h3>CITY</h3>
+              <img src={`url(${partlyCloudyIcon})`}  alt='partlyCloudyIcon' />
+              <h3>Cloudy</h3>
+            </div>
+            <div className="temperature">
+              <h1>28 °F</h1>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
